0e63e73afb784214b641ad4b53f56019cdd243e7
eric <rudion83@gmail.com>
2025-03-27 22:34:26 -0300

    versão 2.1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+function salvarNomeUsuario() {
+    let user = document.getElementById('user');
+    let userName = user.value.trim();
+    
+    if (userName === "") {
+        alert("Ei, aventureiro(a), você precisa dizer seu nome antes de entrar!");
+    } else {
+        alert(`${userName}? Quem se chama assim, com cerveja pode entrar aqui.`);
+        localStorage.setItem("user", userName);
+        window.location.href = '/html/menus.html'; // Redireciona para a página principal
+    }
+}
+
+function exibirNomeUsuario() {
+    let user = localStorage.getItem("user");
+    let result = document.getElementById("result");
+    if (user) {
+        result.innerHTML = `${user},`;
+    } else {
+        result.innerHTML = "Visitante!";
+    }
+}
+
+
 function playone() {
     alert('O taverneiro pigarreia olhando de cara feia para o bardo, como quem diz "Oras, eu to pagando uma diária e você não toca nada? E então, o bardo lhe dá um sorriso sem graça, pega seu bandolim e começa a dedilhar as cordas em busca de inspiração...')
     let music = document.getElementById("music");
@@ -6,33 +30,6 @@ function playone() {
     }
 
 }
-
-// function username() {
-//     let username = prompt('Qual seu nome, aventureiro(a)? ')
-//     let result = document.getElementById('result')
-//     if (username) {
-//     alert(`${username}? Certamente você é novo por aqui...`)
-//     result.innerText = `Seja muito bem-vindo(a) à Biblioteca da Taverna, ${username} ! Pegue uma caneca, acomode-se perto do fogo e escolha um tópico do menu acima para começar sua exploração. Mas antes, clique no play para que o bardo embale sua jornada com uma bela melodia.`
-//     }
-// }
-
-function username() {
-    let user = document.getElementById('user'); // Obtém o elemento input
-    let userName = user.value.trim(); // Obtém o valor do input e remove espaços extras
-
-    if (userName === "") {
-        alert("Ei, aventureiro(a), você precisa dizer seu nome antes de entrar!");
-    } else {
-        alert(`${userName}? Quem se chama assim, com cerveja pode entrar aqui.`);
-        window.location.href = '/html/menus.html'; // Redireciona para a página principal
-    }
-}
-
-// function username2 () {
-//     let result = document.getElementById('result')
-//     result.innerText = `O taverneiro te olha entrar e diz: Seja muito bem-vindo(a) à Biblioteca da Taverna, ${user}! Pegue uma caneca, acomode-se perto do fogo e escolha um tópico do menu acima para começar sua exploração. Mas antes, clique no play para que o bardo embale sua jornada com uma bela melodia.`
-// }
-
 
 
 function pauseone() {
