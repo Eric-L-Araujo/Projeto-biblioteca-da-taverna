a3f7294469cbcd909bef5699d2b8e0c1d560a469
eric <rudion83@gmail.com>
2025-05-27 17:22:29 -0300

    correção de erro no link das páginas

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function salvarNomeUsuario() {
     } else {
         alert(`${userName}? Quem se chama assim, com cerveja pode entrar aqui.`);
         localStorage.setItem("nome", userName);
-        window.location.href = '/html/home.html'; // Redireciona para a página principal
+        window.location.href = '../html/home.html'; // Redireciona para a página principal
     }
 }
 
@@ -24,21 +24,21 @@ function exibirNomeUsuario() {
 exibirNomeUsuario();
 
 
-function playone() {
-    alert('O taverneiro pigarreia olhando de cara feia para o bardo, como quem diz "Oras, eu to pagando uma diária e você não toca nada? E então, o bardo lhe dá um sorriso sem graça, pega seu bandolim e começa a dedilhar as cordas em busca de inspiração...')
-    let music = document.getElementById("music");
-    if (music.paused) {
-        music.play();
-    }
+// function playone() {
+//     alert('O taverneiro pigarreia olhando de cara feia para o bardo, como quem diz "Oras, eu to pagando uma diária e você não toca nada? E então, o bardo lhe dá um sorriso sem graça, pega seu bandolim e começa a dedilhar as cordas em busca de inspiração...')
+//     let music = document.getElementById("music");
+//     if (music.paused) {
+//         music.play();
+//     }
 
-}
+// }
 
-function pauseone() {
-    alert('O taverneiro sobe para a biblioteca, distraído com um livro que fala sobre piratas. Nesse momento, o bardo para de tocar, chega até o bar, pega uma caneca e a enche de cerveja preta em um barril de madeira')
+// function pauseone() {
+//     alert('O taverneiro sobe para a biblioteca, distraído com um livro que fala sobre piratas. Nesse momento, o bardo para de tocar, chega até o bar, pega uma caneca e a enche de cerveja preta em um barril de madeira')
 
-    let music = document.getElementById("music");
+//     let music = document.getElementById("music");
 
-    if (music.play) {
-        music.pause();
-    }
-}
+//     if (music.play) {
+//         music.pause();
+//     }
+// }
